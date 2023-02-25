5fa625c4c39c7cd37e9ff965d21294707250f044
benoit1906 <bevidotto@gmail.com>
2023-02-25 14:43:05 +0100

    add stylecop and change files accordingly

--- WeatherForecastApp/Controllers/WeatherForecastController.cs
+++ WeatherForecastApp/Controllers/WeatherForecastController.cs
@@ -1,23 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
+// <copyright file="WeatherForecastController.cs" company="MyTopCompany">
+// Copyright (c) MyTopCompany. All rights reserved.
+// </copyright>
 
 namespace WeatherForecastApp.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Contains the definition of an object of tye WeatherForecastController.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeatherForecastController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
+        /// <summary>
+        /// Gets all the weather forecasts.
+        /// </summary>
+        /// <returns>A list of weather forecasts.</returns>
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -25,7 +40,7 @@ namespace WeatherForecastApp.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
             })
             .ToArray();
         }
--- WeatherForecastApp/Program.cs
+++ WeatherForecastApp/Program.cs
@@ -1,8 +1,12 @@
+// <copyright file="Program.cs" company="MyTopCompany">
+// Copyright (c) MyTopCompany. All rights reserved.
+// </copyright>
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
