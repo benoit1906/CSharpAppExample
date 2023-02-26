98a9275d73beb34c66d53fdc0f8b069425c30ba2
benoit1906 <bevidotto@gmail.com>
2023-02-26 13:07:01 +0100

    Add datacontract to viewmodels and stylecop to core project

--- WeatherForecastApp.Site/ViewModels/WeatherForecast.cs
+++ WeatherForecastApp.Site/ViewModels/WeatherForecast.cs
@@ -4,29 +4,36 @@
 
 namespace WeatherForecastApp.Site.ViewModels
 {
+    using System.Runtime.Serialization;
+
     /// <summary>
     /// Contains the definition of an object of type <see cref="WeatherForecast"/>.
     /// </summary>
+    [DataContract]
     public class WeatherForecast
     {
         /// <summary>
         /// Gets or sets the date.
         /// </summary>
+        [DataMember(Name = "date")]
         public DateTime Date { get; set; }
 
         /// <summary>
         /// Gets or sets the temperature in Celsius.
         /// </summary>
+        [DataMember(Name = "temperatureC")]
         public int TemperatureC { get; set; }
 
         /// <summary>
         /// Gets the temperature in Farhenheit.
         /// </summary>
+        [DataMember(Name = "temperatureF")]
         public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
 
         /// <summary>
         /// Gets or sets the description.
         /// </summary>
+        [DataMember(Name = "description")]
         public string? Description { get; set; }
     }
 }
