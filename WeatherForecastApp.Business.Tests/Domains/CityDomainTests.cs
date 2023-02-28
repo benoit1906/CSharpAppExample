4b34b593e041fe034e2fb0e978b7f1daa2b7b7d6
benoit1906 <bevidotto@gmail.com>
2023-02-28 22:26:03 +0100

    Adds Business test project, tests addCities method with inline data, member data or class data and makes use of a fake repository through its interface.

---
+++ WeatherForecastApp.Business.Tests/Domains/CityDomainTests.cs
@@ -0,0 +1,108 @@
+﻿// <copyright file="CityDomainTests.cs" company="MyTopCompany">
+// Copyright (c) MyTopCompany. All rights reserved.
+// </copyright>
+
+namespace WeatherForecastApp.Business.Tests.Domains
+{
+    using System.Collections;
+    using WeatherForecastApp.Business.Domains;
+    using WeatherForecastApp.Core.Models;
+
+    /// <summary>
+    /// Contains the definition of the class <see cref="CityDomainTests"/>.
+    /// </summary>
+    public class CityDomainTests
+    {
+        /// <summary>
+        /// Creates a list of values that are tested thanks to the memberData decorator
+        /// in the function ShouldThrowExceptionForInvalidLatOrLong.
+        /// </summary>
+        /// <param name="offset">The offset to test the longitude and latitude.</param>
+        /// <returns>A list of objects to test.</returns>
+        public static IEnumerable<object[]> GetListOfCities(int offset)
+        {
+            var data = new List<object[]>()
+            {
+                new object[] { -5, 50 + offset, "Latitude not valid", },
+                new object[] { 5, -189 + offset, "Longitude not valid", },
+            };
+            return data;
+        }
+
+        /// <summary>
+        /// Tests that an exception is thrown if latitude or longtitude are not valid when adding cities.
+        /// </summary>
+        /// <param name="latitude">The latitude.</param>
+        /// <param name="longitude">The longitude.</param>
+        /// <param name="expectedErrorMessage">The expected error message.</param>
+        // [InlineData(-5, 50, "Latitude not valid")]
+        // [InlineData(5, -189, "Longitude not valid")]
+        [MemberData(nameof(GetListOfCities), 2)]
+        [MemberData(nameof(GetListOfCities), 3)]
+        // [ClassData(typeof(ListOfCities))]
+        [Theory]
+        [Trait("Cities", "Latitude or longitude")]
+        public void ShouldThrowExceptionForInvalidLatOrLong(int latitude, int longitude, string expectedErrorMessage)
+        {
+            // Arrange
+            var cityRepository = new FakeCityRepository();
+            var cityDomain = new CityDomain(cityRepository);
+            IEnumerable<City> cities = new List<City>()
+                {
+                    new City()
+                    {
+                        Id = 9,
+                        Name = "Boussu",
+                        Latitude = latitude,
+                        Longitude = longitude,
+                    },
+                };
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => cityDomain.AddCities(cities));
+
+            // Assert
+            Assert.Equal(expectedErrorMessage, exception.Message);
+        }
+    }
+
+    /// <summary>
+    /// Contains the definition of the class <see cref="ListOfCities"/>.
+    /// This class allows to test the method ShouldThrowExceptionForInvalidLatOrLong
+    /// with the classData decorator.
+    /// </summary>
+    public class ListOfCities : IEnumerable<object[]>
+    {
+        private int offset = 2;
+
+        /// <summary>
+        /// Gets a list of objects to the the method ShouldThrowExceptionForInvalidLatOrLong.
+        /// </summary>
+        /// <returns>A list of objects for tests.</returns>
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var offset = this.GetOffset();
+
+            yield return new object[] { -5, 50 + offset, "Latitude not valid" };
+            yield return new object[] { 5, -189 + offset, "Longitude not valid" };
+        }
+
+        /// <summary>
+        /// Gets the enumerator for IEnumerable.
+        /// </summary>
+        /// <returns>The enumerator.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the offset.
+        /// </summary>
+        /// <returns>The offset.</returns>
+        public int GetOffset()
+        {
+            return this.offset;
+        }
+    }
+}
---
+++ WeatherForecastApp.Business.Tests/FakeCityRepository.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="FakeCityRepository.cs" company="MyTopCompany">
+// Copyright (c) MyTopCompany. All rights reserved.
+// </copyright>
+
+namespace WeatherForecastApp.Business.Tests
+{
+    using WeatherForecastApp.Core.Models;
+    using WeatherForecastApp.Data.Interfaces;
+
+    /// <summary>
+    /// Contains the definition of a class of type <see cref="FakeCityRepository"/>.
+    /// The class allows to use the repository during tests without altering the database.
+    /// </summary>
+    public class FakeCityRepository : ICityRepository
+    {
+        /// <inheritdoc/>
+        public void AddCities(IEnumerable<City> cities)
+        {
+            Console.WriteLine("Saving to Json Database");
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<City> GetAllCities()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<City> SetIdentifier(IEnumerable<City> existingCities, IEnumerable<City> toAddCities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
